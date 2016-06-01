79bbcd15081f48910609c841c17232ff2b97c09a
Matteo Nardini <matteo.shalen@gmail.com>
2016-06-01 20:22:13 +0200

    DOCumentation for ap2016.entities.News,NewsChannel,Role

--- src/ap2016/entities/Role.java
+++ src/ap2016/entities/Role.java
@@ -1,12 +1,18 @@
 package ap2016.entities;
 
 /**
- * Enumerator used to handle the users' roles.
+ * Enumerator used to handle the roles of the user.
  * @author Matteo Nardini
  *
  */
 public enum Role {
 	
-	READ, SEARCH, ADD_NEWS, DELETE_NEWS, IMPORT_NEWS_FROM_FILE, MANAGE_USER, EDIT_NEWS;
+	READ, 						// Basic permission, allows the user to read the news
+	SEARCH, 					// Basic permission, allows the user to search/query the news
+	ADD_NEWS, 					// Allows the user to add new news
+	DELETE_NEWS, 				// Allows the user to delete news
+	EDIT_NEWS,					// Allows the user to edit existing news
+	IMPORT_NEWS_FROM_FILE, 		// Allows the user to import news from another file
+	MANAGE_USER; 				// ADMIN level permission, allows the user to manage both its own roles and the one of the other users.
 	
 }
