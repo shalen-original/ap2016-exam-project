bf8327682ac8e38bedbc474f47f90dbf565dbde1
Matteo Nardini <matteo.shalen@gmail.com>
2016-06-01 18:59:42 +0200

    DOCumentation written for: ap2016.application.*

--- src/ap2016/application/ApplicationConstants.java
+++ src/ap2016/application/ApplicationConstants.java
@@ -14,42 +14,56 @@ import javax.swing.border.Border;
 public class ApplicationConstants {
 
 	/**
-	 * The regular expression used to validate usernames
+	 * The regular expression used to validate usernames.
 	 */
 	public final static Pattern usernameRegEx = Pattern.compile("([a-zA-Z0-9_,'-]*)([.]?)([a-zA-Z0-9_,'-]*)");
 	
 	/**
-	 * The regular expression used to validate passwords
+	 * The regular expression used to validate passwords.
 	 */
 	public final static Pattern passwordRegEx = Pattern.compile(".{8,}");
 	
 	/**
-	 * The regular expression used to validate URLs. The following regular expression has been kindly provided
-	 * by the website: http://stackoverflow.com/questions/163360/regular-expression-to-match-urls-in-java
-	 * The regex is not perfect, but it detects correctly most of the URLs. It could be the case that
-	 * a valid URL is discarded by this RegEx.
+	 * The regular expression used to validate URLs. The following regular expression has been kindly provided by the website: http://stackoverflow.com/questions/163360/regular-expression-to-match-urls-in-java. The regex is not perfect, but it detects correctly most of the URLs. It could be the case that a valid URL is discarded by this RegEx.
 	 */
 	public final static Pattern urlRegEx = Pattern.compile("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
 	
+	/**
+	 * The regular expression used to validate the "language" strings. A language string is expected to be in the format "en-EN", with the two letter before the dash being the minor locale and the to after being the major locale.
+	 */
 	public final static Pattern languageRegEx = Pattern.compile("[a-z]{2}-[A-Z]{2}");
 	
 	
 	/**
-	 * The folder containing all the assets (relative path)
+	 * The folder containing all the assets (relative path).
 	 */
 	public final static String assetsBase = "assets\\" + (System.getenv("TEST_FOLDER") != null ? System.getenv("TEST_FOLDER") : "");
 	
 	/**
-	 * The folder containing all the XML data (relative path)
+	 * The folder containing all the XML data (relative path).
 	 */
 	public final static String dataBase = "data\\" + (System.getenv("TEST_FOLDER") != null ? System.getenv("TEST_FOLDER") : "");
 	
 	
 	
-	
+	/**
+	 * The color used in the program to represent a "valid" situation. 
+	 */
 	public final static Color validColor = new Color(185, 212, 112);
+	
+	/**
+	 * The color used in the program to represent an "invalid" situation.
+	 */
 	public final static Color invalidColor = new Color(236, 160, 150);
+	
+	/**
+	 * The border used in the program to represent a valid input in a component.
+	 */
 	public final static Border validBorder = BorderFactory.createLineBorder(validColor, 2, true);
+	
+	/**
+	 * The border used in the program to represent an invalid input in a component.
+	 */
 	public final static Border invalidBorder = BorderFactory.createLineBorder(invalidColor, 2, true);
 	
 	
--- src/ap2016/application/ApplicationUtilities.java
+++ src/ap2016/application/ApplicationUtilities.java
@@ -4,18 +4,39 @@ import java.util.Arrays;
 
 import javax.swing.JComponent;
 
+/**
+ * This class contains a list of static methods utilities useful in the whole application.
+ * @author Matteo Nardini
+ *
+ */
 public class ApplicationUtilities {
 
+	/**
+	 * This methods uses the regular expression defined in {@link ap2016.application.ApplicationConstants#urlRegEx ApplicationConstants} to validate a given URL.
+	 * @param url The URL to be validated.
+	 * @return The method returns {@code true} if the given URL is valid, {@code false} otherwise.
+	 */
 	public static boolean isValidURL(String url) {
 		return ApplicationConstants.urlRegEx.matcher(url).matches();
 	}
 	
+	/**
+	 * This methods uses the regular expression defined in {@link ap2016.application.ApplicationConstants#languageRegEx ApplicationConstants} to validate a given language string.
+	 * @param language The language string to be validated.
+	 * @return The method returns {@code true} if the given language string is valid, {@code false} otherwise.
+	 */
 	public static boolean isValidLanguage(String language) {
 		return ApplicationConstants.languageRegEx.matcher(language).matches();
 	}
 	
+	/**
+	 * This methods converts a String to an array of byte, in which each byte represents one character of the String. Each character is converted to a byte by using the {@link java.lang.Byte#parseByte(String) Byte.parseByte} method.
+	 * @param str The String to be converted.
+	 * @return The array of byte in which every byte represents a character of the original string.
+	 */
 	public static byte[] stringToByteArray(String str)
 	{
+		// This method practically does the inverse of Arrays.toString(array). However, this method does not works with Strings representing arrays of arrays, because it is not required by this application.
 		if (str.equals("[]"))
 			return new byte[0];
 		
@@ -30,11 +51,21 @@ public class ApplicationUtilities {
 		return ans;
 	}
 	
+	/**
+	 * This methods converts an array of byte to a String, in which every element of the array represent one character of the String.
+	 * @param arr The array to be converted to a String.
+	 * @return The String generated from the given array.
+	 */
 	public static String byteArrayToString(byte[] arr)
 	{
 		return Arrays.toString(arr);
 	}
 	
+	/**
+	 * This method formats a GUI component. If the component is <i>valid</i> then the component is formatted using the {@link ap2016.application.ApplicationConstants#validBorder validBorder} style, otherwise it is formatted with the {@link ap2016.application.ApplicationConstants#invalidBorder invalidBorder}.
+	 * @param c The component to be formatted.
+	 * @param valid If {@code true} the component is considered to be valid, if {@code false} the component is considered to be invalid.
+	 */
 	public static void formatComponent(JComponent c, boolean valid)
 	{
 		if (valid)
